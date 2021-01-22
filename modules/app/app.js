1ddd84e3a7685d88b6fadd33e42a28abf2ef37a6
John Byrne <jbyrne87@gmail.com>
2021-01-22 09:41:52 -0600

    imported client origin to app

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const { CLIENT_ORIGIN } = require('../../config')
 
 const app = express()
 
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}))
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+)
 
 app.use('/people', require('../people/people.router'))
 app.use('/cats', require('../pets/cat.router'))
